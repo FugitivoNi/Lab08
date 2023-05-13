37632f93f27432453889a7c4092836bef4acc602
Fugitivo <104084932+FugitivoNi@users.noreply.github.com>
2023-05-13 00:56:19 -0500

    Update registrarPromocion.php

--- registrarPromocion.php
+++ registrarPromocion.php
@@ -1,4 +1,5 @@
 <?php
+//print_r($_POST);
 if (empty($_POST["txtPromocion"]) || empty($_POST["txtDuracion"])) {
     header('Location: index.php');
     exit();
@@ -7,16 +8,13 @@ if (empty($_POST["txtPromocion"]) || empty($_POST["txtDuracion"])) {
 include_once 'model/conexion.php';
 $promocion = $_POST["txtPromocion"];
 $duracion = $_POST["txtDuracion"];
-$id_persona = $_POST["codigo"];
+$codigo = $_POST["codigo"];
+
 
 $sentencia = $bd->prepare("INSERT INTO promociones(promocion,duracion,id_persona) VALUES (?,?,?);");
-$resultado = $sentencia->execute([$promocion,$duracion, $id_persona]);
+$resultado = $sentencia->execute([$promocion,$duracion, $codigo ]);
 
 if ($resultado === TRUE) {
-    $id_promocion = $bd->lastInsertId();
-    header('Location: agregarPromocion.php?codigo='.$id_persona.'&id_promocion='.$id_promocion);
-} else {
-    header('Location: index.php');
-    exit();
-} 
+    header('Location: agregarPromocion.php?codigo='.$codigo);
+}  
 
